8628f804778836f398eb17ee3307653d8a58b974
popo63301 <popo63301@gmail.com>
2019-01-30 09:24:52 +0100

    get list Wishes when page loads

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors');
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(cors());
 //Setup MongoDB and Mongoose
 
 const mongoose = require('mongoose');
